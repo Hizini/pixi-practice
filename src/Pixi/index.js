8f2616ca1e4b4e6454c53eb80be1a87bab6818a5
KimHeeJin <ejfdnrl@naver.com>
2022-07-14 17:28:52 +0900

    텍스트 추가

--- src/Pixi/index.js
+++ src/Pixi/index.js
@@ -2,24 +2,51 @@ import React, { Component } from 'react'
 import * as PIXI from 'pixi.js'
 import './index.css'
 
+const style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#8bc541'],
+    stroke: '#202020',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440,
+    lineJoin: 'round',
+})
 class Pixi extends Component {
     app = null
     canvas = null
-    canvasRoot = null
-    backgroundImage = null
     
-
     componentDidMount() {
         this.app = new PIXI.Application({ width: window.screen.width, height: 500, backgroundColor: 0x2c3e50 })
-        this.canvasParent.append(this.app.view)
+        this.canvas.append(this.app.view)
         let sprite = PIXI.Sprite.from('https://pixijs.io/examples/examples/assets/bunny.png')
         this.app.stage.addChild(sprite)
 
         let elapsed = 0.0
         this.app.ticker.add((delta) => {
-          elapsed += delta;
-          sprite.x = 500.0 + Math.cos(elapsed/100.0) * 500.0;
+          elapsed += delta
+          sprite.x = 500.0 + Math.cos(elapsed/100.0) * 500.0
+          sprite.y = 250
         })
+
+        const text = new PIXI.Text('hello~~~ everyone~~')
+        const text2 = new PIXI.Text('hello~~~ everyone~~', style)
+        this.app.stage.addChild(text)
+        this.app.stage.addChild(text2)
+        
+        text.skew.set(0.4, -0.3);
+        text.anchor.set(0.5, -0.5);
+        text.x = 1000
+        text.y = 50
+        text2.x = 50
+        text2.y = 100
     }
 
     componentWillUnmount() {
@@ -32,7 +59,7 @@ class Pixi extends Component {
 
         return (
             <div>
-                <div className='canvas' ref={ref => { this.canvasParent = ref }} />
+                <div className='canvas' ref={ref => { this.canvas = ref }} />
             </div>
         )
     }
